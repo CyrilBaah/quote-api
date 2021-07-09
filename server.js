f97319e7d9ba2c72875756374d556cad2d94273f
CyrilBaah <cyril.baah@amalitech.org>
2021-07-09 11:01:48 +0000

    Quotes Features

--- server.js
+++ server.js
@@ -1,10 +1,56 @@
 const express = require('express');
 const app = express();
 
-const { quotes } = require('./data');
-const { getRandomElement } = require('./utils');
+const {
+    quotes
+} = require('./data');
+const {
+    getRandomElement
+} = require('./utils');
 
 const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.get('/api/quotes/random', (req, res, next) => {
+    res.send({
+        quote: getRandomElement(quotes)
+    });
+});
+
+app.get('/api/quotes', (req, res, next) => {
+    if (!req.query.person) {
+        res.send({
+            quotes: quotes
+        });
+        return;
+    }
+
+    const personalQuotes = quotes
+        .filter(quote => quote.person === req.query.person);
+
+    res.send({
+        quotes: personalQuotes
+    });
+})
+
+app.post('/api/quotes', (req, res, next) => {
+    if (!req.query.person || !req.query.quote) {
+        res.status(400).send();
+        return;
+    }
+
+    const newQuote = {
+        quote: req.query.quote,
+        person: req.query.person
+    }
+
+    quotes.push(newQuote);
+    res.status(201).send({
+        quote: newQuote
+    });
+});
+
+app.listen(PORT, (req, res) => {
+    console.log(`Server is listening on PORT: ${PORT}`);
+});
